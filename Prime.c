25c489d29fcb9425aeb56ee1d1735905dca6292b
mantu786 <hemantpr75@gmail.com>
2020-09-20 17:05:00 +0530

    prime prog added

---
+++ Prime.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+// using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i=2; i*i <= n; i++){
+        if(n%2==0){
+            cout << "Not Prime";
+            break;
+        }else{
+            cout << "Prime";
+        }
+    }
+    return 0;
+}
